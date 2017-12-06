145cf47e1217f2043140ed8717ed07e916bea436
cjohnson73 <cjohnson73@murraystate.edu>
2017-12-06 15:55:34 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,17 +9,30 @@ int main()
   string text;
   getline(cin, text);
   string output = "++++++++++[";
+  int uniqs = 0;
+  int tensArr[text.length()][2];
   for(unsigned int j = 0; j<text.length(); j++)
   {
-      if(j<text.length()-1 && text.at(j)==text.at(j+1))
-        continue;
       char c = text.at(j);
-      output += ">";
       int tens = (int)round(((float)c)/10);
+      bool uni = true;
+      for(unsigned int i = 0; i<uniqs; i++)
+      {
+        if(tensArr[i]==tens)
+        {
+          uni = false;
+          break;
+        }
+      }
+      if(!uni)
+        continue;
+      tensArr[uniqs++][0] = tens;
+      tensArr[uniqs][1] = uniqs;
+      output += ">";
       for(int i = 0; i<tens; i++)
         output += "+";
   }
-  output += "[<]>-]";
+  output += "[<]>-]";//stopping here for now   currnet state won't work
   for(unsigned int j = 0; j<text.length(); j++)
   {
       char c = text.at(j);
